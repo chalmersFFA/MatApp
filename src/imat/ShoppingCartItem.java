7a952689ec6697e1bfde96df6fb073ca45f9ba71
teamkoregbgb <teamkoregbgb@gmail.com>
2018-05-07 14:50:53 +0200

    påbörjat carten

---
+++ src/imat/ShoppingCartItem.java
@@ -0,0 +1,29 @@
+package imat;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.ShoppingItem;
+
+import java.io.IOException;
+
+/**
+ * Created by Jonathan Köre on 2018-05-07.
+ */
+public class ShoppingCartItem extends AnchorPane {
+    ShoppingCartController parentController;
+
+    public ShoppingCartItem(ShoppingItem shoppingItem, ShoppingCartController parentController) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("steg1_betalning_item.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+
+        this.parentController = parentController;
+    }
+}
