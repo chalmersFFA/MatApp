f79d2573908fd6f12d248b3bb9741658d808a97d
teamkoregbgb <teamkoregbgb@gmail.com>
2018-05-08 11:30:24 +0200

    kassafunktioner

--- src/imat/ShoppingCartController.java
+++ src/imat/ShoppingCartController.java
@@ -4,13 +4,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
-import se.chalmers.cse.dat216.project.CartEvent;
-import se.chalmers.cse.dat216.project.IMatDataHandler;
-import se.chalmers.cse.dat216.project.ShoppingCart;
-import se.chalmers.cse.dat216.project.ShoppingCartListener;
+import se.chalmers.cse.dat216.project.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Jonathan Köre on 2018-05-07.
@@ -21,6 +20,7 @@ public class ShoppingCartController extends AnchorPane implements ShoppingCartLi
     private IMatDataHandler db = IMatDataHandler.getInstance();
     private ShoppingCart shoppingCart = db.getShoppingCart();
     private ArrayList<ShoppingCartItem> item_list = new ArrayList<>();
+    private Map<String, ShoppingCartItem> shoppingCartItemMap = new HashMap<String, ShoppingCartItem>();
 
     @FXML
     FlowPane shoppingCartFlowPane;
@@ -39,10 +39,28 @@ public class ShoppingCartController extends AnchorPane implements ShoppingCartLi
         this.parentController = parentController;
         shoppingCart.addShoppingCartListener(this);
 
+        initShoppingCartItems();
+        test();
+
+    }
+
+    private void initShoppingCartItems() {
+        for (Product p : db.getProducts(ProductCategory.BERRY)){
+            shoppingCartItemMap.put(p.getName(), new ShoppingCartItem(new ShoppingItem(p), this));
+        }
     }
 
     @Override
     public void shoppingCartChanged(CartEvent cartEvent) {
+        /*ShoppingItem shoppingItem = cartEvent.getShoppingItem();
+        if(item_list.contains(shoppingCartItemMap.get(shoppingItem.getProduct().getName()))) {
+            shoppingCartItemMap.get(shoppingItem.getProduct().getName()).update();
+        }*/
+    }
 
+    private void test() {
+        //item_list.add( new ShoppingCartItem(new ShoppingItem(db.getProduct(87)), this));
+        shoppingCartFlowPane.getChildren().clear();
+        shoppingCartFlowPane.getChildren().add(new ShoppingCartItem(new ShoppingItem(db.getProduct(87)), this));
     }
 }
--- src/imat/ShoppingCartItem.java
+++ src/imat/ShoppingCartItem.java
@@ -1,7 +1,15 @@
 package imat;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.io.IOException;
@@ -11,6 +19,21 @@ import java.io.IOException;
  */
 public class ShoppingCartItem extends AnchorPane {
     ShoppingCartController parentController;
+    ShoppingItem shoppingItem;
+    IMatDataHandler db = IMatDataHandler.getInstance();
+
+    @FXML
+    ImageView productImageView;
+    @FXML
+    Label productLabel;
+    @FXML
+    Label priceLabel;
+    @FXML
+    Button increaseButton;
+    @FXML
+    Button decreaseButton;
+    @FXML
+    TextField amountTextField;
 
     public ShoppingCartItem(ShoppingItem shoppingItem, ShoppingCartController parentController) {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/steg1_betalning_item.fxml"));
@@ -22,8 +45,40 @@ public class ShoppingCartItem extends AnchorPane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
-
-
         this.parentController = parentController;
+        this.shoppingItem = shoppingItem;
+        priceLabel.setText(Double.toString(shoppingItem.getProduct().getPrice()));
+        productImageView.setImage(db.getFXImage(this.shoppingItem.getProduct()));
+        productLabel.setText(this.shoppingItem.getProduct().getName());
+
+        amountTextField.textProperty().addListener(new ChangeListener<String>() {
+
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                setAmount(Double.parseDouble(newValue));
+            }
+        });
+        updateTextField();
+    }
+
+    public void updateTextField() {
+        amountTextField.setText(Double.toString(shoppingItem.getAmount()));
+        priceLabel.setText(Double.toString(shoppingItem.getTotal()));
+    }
+    @FXML
+    public void increaseAmount() {
+        setAmount(shoppingItem.getAmount() + 1);
+    }
+    @FXML
+    public void decreaseAmount() {
+        setAmount(shoppingItem.getAmount()-1);
+    }
+
+    public void setAmount(double i) {
+        if (i < 0)
+            shoppingItem.setAmount(0);
+        else
+            shoppingItem.setAmount(i);
+        updateTextField();
     }
 }
