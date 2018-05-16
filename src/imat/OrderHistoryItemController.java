ca40a918441021f1f7e596c75ea52879ed16f6f0
chalmersFFA <38913046+chalmersFFA@users.noreply.github.com>
2018-05-16 14:00:43 +0200

    fixade pliancy på historyItems samt la till en label för totalt pris på en gammal order.

--- src/imat/OrderHistoryItemController.java
+++ src/imat/OrderHistoryItemController.java
@@ -2,6 +2,7 @@ package imat;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
@@ -15,7 +16,7 @@ import java.util.Map;
 /**
  * Created by Jonathan Köre on 2018-05-09.
  */
-public class OrderHistoryItemController extends VBox{
+public class OrderHistoryItemController extends AnchorPane{
 
     private ShoppingCartController shoppingCartController;
     private IMatController parentController;
@@ -27,6 +28,8 @@ public class OrderHistoryItemController extends VBox{
     FlowPane orderHistoryItemFlowPane;
     @FXML
     Label orderNameLabel;
+    @FXML
+    Label total;
 
 
     public OrderHistoryItemController(IMatController parentController, ShoppingCartController shoppingCartController, Order order) {
@@ -43,6 +46,13 @@ public class OrderHistoryItemController extends VBox{
             throw new RuntimeException(exception);
         }
         orderNameLabel.setText(order.getDate().toString());
+        double totalAmount = 0;
+        for(ShoppingItem item : order.getItems()){
+            totalAmount += item.getTotal();
+        }
+        total.setText("Totalt: " + Double.toString(totalAmount) + " kr");
+        //this.getChildren().get(0).getStyleClass().remove("historyItem");
+
 
     }
 
@@ -63,9 +73,11 @@ public class OrderHistoryItemController extends VBox{
         for (ShoppingItem s : order.getItems()) {
             orderHistoryItemFlowPane.getChildren().add(new OrderHistorySubItem(s));
         }
+        this.getChildren().get(0).getStyleClass().add("historyItemExpanded");
     }
     public void collapse(){
         orderHistoryItemFlowPane.getChildren().clear();
+        this.getChildren().get(0).getStyleClass().remove("historyItemExpanded");
     }
 
 
