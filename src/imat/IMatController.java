2484cb3448c98131918e3900ac9d39b524e2592b
teamkoregbgb <teamkoregbgb@gmail.com>
2018-05-16 16:12:53 +0200

    sökfunktion

--- src/imat/IMatController.java
+++ src/imat/IMatController.java
@@ -9,9 +9,12 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.util.Duration;
 import se.chalmers.cse.dat216.project.*;
@@ -19,10 +22,7 @@ import se.chalmers.cse.dat216.project.*;
 import javax.tools.Tool;
 import java.lang.reflect.Field;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.*;
 
 /**
  * Created by Jonathan Köre on 2018-05-03.
@@ -91,6 +91,9 @@ public class IMatController extends VBox implements Initializable {
     @FXML
     AnchorPane headerAnchorPane;
 
+    @FXML
+    TextField searchTextField;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         myDetails = new MyDetails(this);
@@ -107,11 +110,13 @@ public class IMatController extends VBox implements Initializable {
         updateProductList(ProductCategory.BERRY);
         shoppingCart.fireShoppingCartChanged(null, false);
         orderHistoryController.createHistory();
+        setGlobalEventHandler(searchTextField);
+
     }
 
     private void initProducts() {
         ShoppingCartItem s;
-        for (Product p : db.getProducts(ProductCategory.BERRY)) {
+        for (Product p : db.getProducts()) {
             //ItemHandler itemHandler = new ItemHandler(new ShoppingItem(p,0));
             storeListItemMap.put(p.getName(), new StoreListItem(p, this));
             shoppingCartController.addToHashMap(new ShoppingCartItem(p, shoppingCartController));
@@ -122,15 +127,17 @@ public class IMatController extends VBox implements Initializable {
 
 
     public void updateProductList(ProductCategory category) {
+        updateProductListLoop(db.getProducts(category));
+        currentSiteLabel.setText("Kategori: " + Translator.swe(category));
+    }
 
+    private void updateProductListLoop(List<Product> products) {
         mainFlowPane.getChildren().clear();
-        for (Product p : db.getProducts(category)) {
+        for(Product p : products) {
             mainFlowPane.setHgap(30);
             mainFlowPane.setVgap(10);
             mainFlowPane.setPadding(new Insets(30, 10, 10, 10));
             mainFlowPane.getChildren().add(storeListItemMap.get(p.getName()));
-            currentSiteLabel.setText("Kategori: " + Translator.swe(category));
-
         }
     }
 
@@ -144,6 +151,15 @@ public class IMatController extends VBox implements Initializable {
         //favouriteLabel.setId("current");
         currentSiteLabel.setText("Mina Favoritvaror");
 
+    }
+
+    private void setGlobalEventHandler(Node root) {
+        root.addEventHandler(KeyEvent.KEY_PRESSED, ev -> {
+            if (ev.getCode() == KeyCode.ENTER) {
+                search();
+                ev.consume();
+            }
+        });
     }
 
     @FXML
@@ -391,6 +407,21 @@ public class IMatController extends VBox implements Initializable {
     }
     public void refreshHistory(){
         orderHistoryController.createHistory();
+    }
+
+    @FXML
+    private void search() {
+        List<Product> p = db.findProducts(searchTextField.getText());
+        if(p.size() == 0) {
+            mainFlowPane.getChildren().clear();
+            currentSiteLabel.setText("Sökningen " + "\"" + searchTextField.getText() + "\" gav inga resultat.");
+        }
+        else {
+            updateProductListLoop(db.findProducts(searchTextField.getText()));
+            currentSiteLabel.setText("Sökresultat för: " + searchTextField.getText());
+        }
+
+
     }
 
 }
