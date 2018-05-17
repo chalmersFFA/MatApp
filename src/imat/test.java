0433604a4ce9557b143c79f8c668364f9c30dd4a
robertzetterlund <robertz@student.chalmers.se>
2018-05-17 21:36:16 +0200

    Skapade en cooling som ändrar en double typ 1234.44 till 1 234.44 (som en string)

--- src/imat/test.java
+++ src/imat/test.java
@@ -13,6 +13,22 @@ public class test {
         IMatDataHandler db = IMatDataHandler.getInstance();
         Product p = db.getProduct(87);
         System.out.println(p.getName());
+
+        System.out.println(fixNumber(1234567.44));
     }
 
+    public static String fixNumber(double number) {
+        String strNum = Double.toString(number);
+        StringBuilder sb = new StringBuilder();
+        int p = 0;
+        for(int i = strNum.length(); i > 0; i--) {
+            sb.append(strNum.charAt(i-1));
+            p++;
+            if(p % 3 == 0 && strNum.charAt(i-1)!='.'){
+                sb.append(" ");
+            }
+        }
+        sb = sb.reverse();
+        return sb.toString();
+    }
 }
