fed34c640615a614baade5836ee5ef9d75b9f415
robertzetterlund <robertz@student.chalmers.se>
2018-05-20 12:53:04 +0200

    ....

--- src/imat/HelpPage.java
+++ src/imat/HelpPage.java
@@ -2,14 +2,24 @@ package imat;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.scene.Scene;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Locale;
 
-public class HelpPage extends AnchorPane{
+public class HelpPage extends AnchorPane {
     @FXML
     Label titleLabel;
     @FXML
@@ -29,6 +39,5 @@ public class HelpPage extends AnchorPane{
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
-        this.parentController = parentController;
     }
 }
