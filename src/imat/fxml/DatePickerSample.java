9b833d2ff0bb7f2cbd342664cb0ba413d8ac4146
robertzetterlund <robertz@student.chalmers.se>
2018-05-21 10:32:26 +0200

    Ändrade iMat Loggan

--- src/imat/fxml/DatePickerSample.java
+++ src/imat/fxml/DatePickerSample.java
@@ -1,4 +1,6 @@
-package imat;import java.time.LocalDate;
+package imat.fxml;
+
+import java.time.LocalDate;
 import java.util.Locale;
 import javafx.application.Application;
 import javafx.geometry.HPos;
@@ -10,18 +12,18 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Callback;
-
+ 
 public class DatePickerSample extends Application {
-
+ 
     private Stage stage;
-    //private DatePicker checkInDatePicker;
+    private DatePicker checkInDatePicker;
     private DatePicker checkOutDatePicker;
-
+ 
     public static void main(String[] args) {
-        Locale.setDefault(Locale.UK);
+        Locale.setDefault(Locale.US);                  
         launch(args);
     }
-
+ 
     @Override
     public void start(Stage stage) {
         this.stage = stage;
@@ -29,34 +31,44 @@ public class DatePickerSample extends Application {
         initUI();
         stage.show();
     }
-
+ 
     private void initUI() {
         VBox vbox = new VBox(20);
         vbox.setStyle("-fx-padding: 10;");
         Scene scene = new Scene(vbox, 400, 400);
         stage.setScene(scene);
+        checkInDatePicker = new DatePicker();
         checkOutDatePicker = new DatePicker();
-        final Callback<DatePicker, DateCell> dayCellFactory = new Callback<DatePicker, DateCell>() {
-                    @Override
-                    public DateCell call(final DatePicker datePicker) {
-                        return new DateCell() {
-                            @Override
-                            public void updateItem(LocalDate item, boolean empty) {
-                                super.updateItem(item, empty);
-                                if (item.isBefore(LocalDate.now())) {
+        checkInDatePicker.setValue(LocalDate.now());
+        final Callback<DatePicker, DateCell> dayCellFactory = 
+            new Callback<DatePicker, DateCell>() {
+                @Override
+                public DateCell call(final DatePicker datePicker) {
+                    return new DateCell() {
+                        @Override
+                        public void updateItem(LocalDate item, boolean empty) {
+                            super.updateItem(item, empty);
+                           
+                            if (item.isBefore(
+                                    checkInDatePicker.getValue().plusDays(1))
+                                ) {
                                     setDisable(true);
                                     setStyle("-fx-background-color: #ffc0cb;");
-                                }
-                            }
-                        };
+                            }   
                     }
                 };
+            }
+        };
         checkOutDatePicker.setDayCellFactory(dayCellFactory);
-        checkOutDatePicker.setValue(LocalDate.now().plusDays(1));
+        checkOutDatePicker.setValue(checkInDatePicker.getValue().plusDays(1));
         GridPane gridPane = new GridPane();
         gridPane.setHgap(10);
         gridPane.setVgap(10);
-        Label checkOutlabel = new Label("Vilken dag vill du bli levererad varorna?");
+        Label checkInlabel = new Label("Check-In Date:");
+        gridPane.add(checkInlabel, 0, 0);
+        GridPane.setHalignment(checkInlabel, HPos.LEFT);
+        gridPane.add(checkInDatePicker, 0, 1);
+        Label checkOutlabel = new Label("Check-Out Date:");
         gridPane.add(checkOutlabel, 0, 2);
         GridPane.setHalignment(checkOutlabel, HPos.LEFT);
         gridPane.add(checkOutDatePicker, 0, 3);
